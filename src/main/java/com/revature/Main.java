ea1a5bb40dc64d546b805ec9feecc8ee7afea090
Raymond <raymondxia95@gmail.com>
2018-03-06 13:50:32 -0500

    Added a controller to the main method

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,5 +1,10 @@
 package com.revature;
 
+import com.revature.controller.BankController;
+import com.revature.model.User;
+import com.revature.service.BankService;
+import com.revature.view.BankView;
+
 /**
  * Create an instance of your controller and launch your application.
  *
@@ -8,6 +13,19 @@ package com.revature;
 public class Main {
 
     public static void main(String[] args) {
+
+	User user = new User();
+	BankService bankService = new BankService();
+	BankView view = new BankView();
+
+	BankController controller = new BankController(user, bankService, view);
+	controller.login();
+	controller.getBalance();
+	controller.widthdraw();
+	controller.getBalance();
+	controller.deposit();
+	controller.getBalance();
+	controller.logout();
 
     }
 }
