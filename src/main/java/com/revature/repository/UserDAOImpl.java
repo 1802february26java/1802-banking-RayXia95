5f07bb7fd8d553e63fc8fd81f3e86fee03e79caa
Raymond <raymondxia95@gmail.com>
2018-03-06 18:41:28 -0500

    Added User DAO implementation & haven't tested the connection yet

--- src/main/java/com/revature/repository/UserDAOImpl.java
+++ src/main/java/com/revature/repository/UserDAOImpl.java
@@ -1,22 +1,86 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.model.User;
 
-
 public class UserDAOImpl implements UserDAO {
+
+    Connection connection = null; // Our connection to the database
+    PreparedStatement stmt = null; // We use prepared statements to help protect against SQL injection
 
     @Override
     public List<User> getAllUser() {
-	// TODO Auto-generated method stub
-	return null;
+	List<User> users = new ArrayList<>();
+
+	try {
+	    connection = DAOUtilities.getConnection(); // Get our database connection from the manager
+	    String sql = "SELECT * FROM Users"; // Our SQL query
+	    stmt = connection.prepareStatement(sql); // Creates the prepared statement from the query
+
+	    ResultSet rs = stmt.executeQuery(); // Queries the database
+
+	    // So long as the ResultSet actually contains results...
+	    while (rs.next()) {
+		// We need to populate a Book object with info for each row from our query result
+		User user = new User();
+
+		// Each variable in our Book object maps to a column in a row from our results.
+		user.setUsername(rs.getString("U_USERNAME)"));
+		user.setPassword(rs.getString("U_PASSWORD"));
+		user.setName(rs.getString("U_NAME"));
+		user.setBalance(rs.getDouble("U_BALANCE"));
+		// Finally we add it to the list of Book objects returned by this query.
+		users.add(user);
+	    }
+
+	    rs.close();
+
+	}
+	catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	finally {
+	    closeResources();
+	}
+	return users;
     }
 
     @Override
     public User getUserByUsername(String username) {
-	// TODO Auto-generated method stub
-	return null;
+	User user = null;
+
+	try {
+	    connection = DAOUtilities.getConnection();
+	    String sql = "SELECT * FROM User WHERE U_USERNAME = ?";
+	    stmt = connection.prepareStatement(sql);
+
+	    stmt.setString(1, username);
+
+	    ResultSet rs = stmt.executeQuery();
+
+	    if (rs.next()) {
+		user = new User();
+		user.setUsername(rs.getString("U_USERNAME)"));
+		user.setPassword(rs.getString("U_PASSWORD"));
+		user.setName(rs.getString("U_NAME"));
+		user.setBalance(rs.getDouble("U_BALANCE"));
+	    }
+
+	}
+	catch (SQLException e) {
+	    e.printStackTrace();
+	}
+	finally {
+	    closeResources();
+	}
+
+	return user;
     }
 
     @Override
@@ -37,4 +101,25 @@ public class UserDAOImpl implements UserDAO {
 	return false;
     }
 
+    private void closeResources() {
+	try {
+	    if (stmt != null) {
+		stmt.close();
+	    }
+	}
+	catch (SQLException e) {
+	    System.out.println("Could not close statement!");
+	    e.printStackTrace();
+	}
+
+	try {
+	    if (connection != null) {
+		connection.close();
+	    }
+	}
+	catch (SQLException e) {
+	    System.out.println("Could not close connection!");
+	    e.printStackTrace();
+	}
+    }
 }
