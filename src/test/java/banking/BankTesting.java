95ca86b769cde1acacd43bf67a3b35261a6d3e89
Raymond <raymondxia95@gmail.com>
2018-03-11 18:52:35 -0400

    Added singleton to DAO and two test cases

---
+++ src/test/java/banking/BankTesting.java
@@ -0,0 +1,39 @@
+package banking;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.revature.model.User;
+import com.revature.repository.UserDAO;
+import com.revature.repository.UserDAOImpl;
+import com.revature.service.BankService;
+
+public class BankTesting {
+
+    private UserDAO userDAO;
+    private User userTest = new User();
+    private BankService bankService = new BankService();
+
+    @Before
+    public void setup() {
+	userDAO = UserDAOImpl.getUserDAO();
+	userTest.setUsername("Admin");
+	userTest.setPassword("admin");
+	userTest.setName("ray");
+	userTest.setBalance(0);
+
+    }
+
+    @Test
+    public void testGetUserFromDB() {
+	assertEquals(userTest, bankService.getUserFromDB("Admin", "admin"));
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void testBalance() {
+	assertEquals(null, bankService.logout(userTest));
+    }
+}
